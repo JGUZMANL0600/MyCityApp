53045e5ee22767d87bac091ee6efc8b8e654ab41
Julio César Guzmán López <guzmanl0600@gmail.com>
2026-02-06 18:18:57 -0600

    Se crea lista de categorias
    Se programo la card individual para cada item de la lista de categorías y se programo la iteración para que aparezca cada una de las categorías en la lista

--- app/src/main/java/com/example/mycity/data/LocalCategoryDataProvider.kt
+++ app/src/main/java/com/example/mycity/data/LocalCategoryDataProvider.kt
@@ -7,46 +7,78 @@ import com.example.mycity.model.SubCategory
 object LocalCategoryDataProvider {
     val defaultCategory= getCategoryData()[0]
 
-    fun getCategoryData() : List<Category> {
-        return listOf(
-            Category(
-                id = 1,
-                name = R.string.cafeterias,
-                categoryIcon = TODO(),
-                subCategories = listOf(SubCategory(
-                    id = 1,
-                    name = R.string.cafe_tacuba,
-                    subCategoryIcon = TODO(),
-                    location = TODO(),
-                    description = TODO()
-                ))
-            ),
-            Category(
-                id = 2,
-                name = R.string.bares,
-                categoryIcon = TODO(),
-                subCategories = TODO()
-            ),
-            Category(
-                id = 3,
-                name = R.string.restaurantes,
-                categoryIcon = TODO(),
-                subCategories = TODO()
-            ),
-            Category(
-                id = 4,
-                name = R.string.parques,
-                categoryIcon = TODO(),
-                subCategories = TODO()
-            ),
-            Category(
-                id = 5,
-                name = R.string.centros_comerciales,
-                categoryIcon = TODO(),
-                subCategories = TODO()
-            )
-
-        )
-
-    }
+   fun getCategoryData() : List<Category> {
+       return listOf(
+           Category(
+               id = 1,
+               name = R.string.cafeterias,
+               categoryIcon = R.drawable.ic_category_cafe,
+               subCategories = listOf(
+                   SubCategory(
+                       id = 1,
+                       name = R.string.cafe_tacuba,
+                       subCategoryIcon = R.drawable.ic_category_cafe,
+                       location = R.string.cafe_tacuba_location,
+                       description = R.string.cafe_tacuba_description
+                   )
+               ),
+           ),
+           Category(
+               id = 2,
+               name = R.string.bares,
+               categoryIcon = R.drawable.ic_category_pub,
+               subCategories = listOf(
+                   SubCategory(
+                       id = 1,
+                       name = R.string.cafe_tacuba,
+                       subCategoryIcon = R.drawable.ic_category_cafe,
+                       location = R.string.cafe_tacuba_location,
+                       description = R.string.cafe_tacuba_description
+                   )
+               ),
+           ),
+           Category(
+               id = 3,
+               name = R.string.restaurantes,
+               categoryIcon = R.drawable.ic_category_restaurant,
+               subCategories = listOf(
+                   SubCategory(
+                       id = 1,
+                       name = R.string.cafe_tacuba,
+                       subCategoryIcon = R.drawable.ic_category_cafe,
+                       location = R.string.cafe_tacuba_location,
+                       description = R.string.cafe_tacuba_description
+                   )
+               ),
+           ),
+           Category(
+               id = 4,
+               name = R.string.parques,
+               categoryIcon = R.drawable.ic_category_park,
+               subCategories = listOf(
+                   SubCategory(
+                       id = 1,
+                       name = R.string.cafe_tacuba,
+                       subCategoryIcon = R.drawable.ic_category_cafe,
+                       location = R.string.cafe_tacuba_location,
+                       description = R.string.cafe_tacuba_description
+                   )
+               ),
+           ),
+           Category(
+               id = 5,
+               name = R.string.centros_comerciales,
+               categoryIcon = R.drawable.ic_category_mall,
+               subCategories = listOf(
+                   SubCategory(
+                       id = 1,
+                       name = R.string.cafe_tacuba,
+                       subCategoryIcon = R.drawable.ic_category_cafe,
+                       location = R.string.cafe_tacuba_location,
+                       description = R.string.cafe_tacuba_description
+                   )
+               ),
+           ),
+       )
+   }
 }
--- app/src/main/java/com/example/mycity/ui/MyCityScreens.kt
+++ app/src/main/java/com/example/mycity/ui/MyCityScreens.kt
@@ -1,13 +1,37 @@
 package com.example.mycity.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mycity.R
+import com.example.mycity.data.LocalCategoryDataProvider
 import com.example.mycity.model.Category
+import com.example.mycity.ui.theme.MyCityTheme
 
 @Composable
 private fun CategoryListItem(
@@ -21,6 +45,101 @@ private fun CategoryListItem(
         shape = RoundedCornerShape(dimensionResource(R.dimen.card_corner_radius)),
         onClick = { onItemClick(category) }
     ){
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .size(dimensionResource(R.dimen.card_image_height))
+
+        ) {
+            CategoryListImageItem(
+                category,
+                modifier = Modifier.size(dimensionResource(R.dimen.card_image_height))
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = stringResource(category.name),
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.padding(bottom = dimensionResource(R.dimen.card_text_vertical_space)),
+
+                )
+
+
+            }
+
+
+        }
+
+    }
+}
+
+
+@Composable
+private fun CategoryListImageItem(
+    category: Category,
+    modifier: Modifier = Modifier
+){
+    Box(
+        modifier=modifier
+    ){
+        Image(
+            painter = painterResource(category.categoryIcon),
+            contentDescription = null,
+            alignment = Alignment.Center,
+            contentScale = ContentScale.FillWidth
+        )
+    }
+}
+
+@Composable
+private fun CategoryList(
+    category:List<Category>,
+    onClick: (Category) -> Unit,
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(0.dp)
+){
+    LazyColumn(
+        contentPadding = contentPadding,
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium)),
+        modifier = modifier.padding(top = dimensionResource(R.dimen.padding_medium)),
+    ){
+        items(items = category, key = {category -> category.id}){ category ->
+            CategoryListItem(
+                category = category,
+                onItemClick = onClick
+            )
+        }
+
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CategoryListItemPreview(){
+    MyCityTheme {
+        CategoryListItem(
+            category = LocalCategoryDataProvider.defaultCategory,
+            onItemClick = {}
+        )
+
+    }
+}
+
+@Preview
+@Composable
+fun CategoryListPreview(){
+    MyCityTheme() {
+        Surface() {
+            CategoryList(
+                category = LocalCategoryDataProvider.getCategoryData(),
+                onClick = {}
+            )
+        }
 
     }
 }
